84c90c173fb6d8ed9f3ccf33282a1fb2bacb4730
Nikil <sdnikil1sn@gmail.com>
2026-07-31 22:46:00 +0530

    feat(database): initialize Drift database

---
+++ lib/database/database.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+
+part 'database.g.dart';
+
+@DriftDatabase()
+class AppDatabase extends _$AppDatabase {
+  AppDatabase() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dir = await getApplicationDocumentsDirectory();
+
+    final file = File(
+      p.join(dir.path, 'lifeos.db'),
+    );
+
+    return NativeDatabase.createInBackground(file);
+  });
+}
